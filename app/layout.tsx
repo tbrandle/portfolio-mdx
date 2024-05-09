93a01c7f8f9bcfa34fc77a9fbd863bf99d9a805f
Tim Brandle <tbrandle53@gmail.com>
2024-05-09 16:31:31 -0600

    add name and links

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,16 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "TB",
+    template: "%s | tbrandle.com",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Project portfolio for Tim Brandle",
   openGraph: {
-    title: "chronark.com",
+    title: "tbrandle.com",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
-    images: [
-      {
-        url: "https://chronark.com/og.png",
-        width: 1920,
-        height: 1080,
-      },
-    ],
+      "Project portfolio for Tim Brandle",
+    url: "https://tbrandle.com",
+    siteName: "tbrandle.com",
     locale: "en-US",
     type: "website",
   },
@@ -38,7 +31,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Tim Brandle",
     card: "summary_large_image",
   },
   icons: {
